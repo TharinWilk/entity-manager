b722260a090a3181798089f1c319090af2f69663
Tharin Wilk <Tharin.m.wilk@gmail.com>
2024-02-11 20:13:23 -0500

    Create base styles and import icons

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="primary">Hello</h1>
+    <h1 class="secondary">Hello</h1>
+    <h1 class="test">Hello</h1>
+    <div style="width: 100%; display: grid; place-items: center">
+      <BaseButton size="xs">
+        <span class="sr-only">Add</span>
+        <Icon name="mdi:plus" size="24" color="white"></Icon>
+      </BaseButton>
+
+      <BaseButton>Hello</BaseButton>
+    </div>
   </div>
 </template>
+
+<style>
+.primary {
+  color: var(--text-primary);
+  font-size: 64px;
+}
+.secondary {
+  color: var(--text-secondary);
+  font-size: 64px;
+}
+.test {
+  color: var(--text-accent);
+  font-size: 64px;
+}
+</style>
---
+++ components/base/Button.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+type Size = "xs" | "sm" | "md" | "lg" | "xl";
+
+const props = defineProps({
+  size: {
+    type: String as PropType<Size>,
+    default: "md",
+  },
+});
+
+const size = computed(() => {
+  switch (props.size) {
+    case "xs":
+      return "1rem";
+      break;
+    case "sm":
+      return "1.5rem";
+      break;
+    case "md":
+      return "2rem";
+      break;
+    case "lg":
+      return "4rem";
+      break;
+    case "xl":
+      return "8rem";
+      break;
+  }
+});
+</script>
+
+<template>
+  <button class="base-button" :style="{ '--size': size }">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.base-button {
+  --size: 1rem;
+
+  display: grid;
+  place-items: center;
+  padding: calc(var(--size) / 4);
+
+  box-shadow: 0px 0px calc(var(--size) / 8) 0px var(--surface-shadow);
+  border-radius: 0.5rem;
+
+  background-color: var(--surface-default);
+
+  transition: 200ms linear;
+
+  &:focus-visible {
+    outline-color: var(--surface-shadow);
+    outline-offset: 2px;
+  }
+
+  &:focus-visible:active {
+    outline-offset: 0px;
+    scale: 1;
+  }
+
+  &:hover {
+    box-shadow: 0px 0px calc(var(--size) / 4) 0px var(--surface-shadow);
+  }
+
+  &:active {
+    transition: 75ms;
+    scale: 0.96;
+    box-shadow: 0px 0px calc(var(--size) / 8) 0px var(--surface-shadow);
+  }
+}
+</style>
