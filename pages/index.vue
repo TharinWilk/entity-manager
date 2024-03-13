fb568a6d8af5f1f0ab8a6a7ee2ba72358bd5a73a
Tharin Wilk <Tharin.m.wilk@gmail.com>
2024-03-12 20:27:12 -0400

    Created card section and draggable and transition logic

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,84 @@
 <script setup lang="ts">
 const managerData = useDataManagerStore();
+
+const dataList = ref<HTMLElement>();
+const isTransitioning = ref(false);
+
+const handleDragSorting = (event: MouseEvent) => {
+  if (!dataList.value) {
+    return;
+  }
+
+  const draggedElement = dataList.value.querySelector(
+    ".card.dragging"
+  ) as HTMLElement;
+
+  const siblings = Array.from(
+    dataList.value.querySelectorAll(".card:not(.dragging)")
+  ) as HTMLElement[];
+
+  const mouseY = event.clientY;
+  const mouseX = event.clientX;
+
+  for (const sibling of siblings) {
+    const siblingRect = sibling.getBoundingClientRect();
+
+    if (
+      mouseY >= siblingRect.top &&
+      mouseY <= siblingRect.bottom &&
+      mouseX >= siblingRect.left &&
+      mouseX <= siblingRect.right
+    ) {
+      if (isTransitioning.value) return;
+
+      const elementKey = draggedElement.dataset.key;
+      const siblingKey = sibling.dataset.key;
+      if (!elementKey || !siblingKey) return;
+
+      updateDataIndexing(elementKey, siblingKey);
+      isTransitioning.value = true;
+
+      setTimeout(() => {
+        isTransitioning.value = false;
+      }, 300);
+    }
+  }
+};
+
+const { data, updateDataIndexing } = useData(managerData);
+
+function useData(inputData: any) {
+  const data = ref(inputData.filteredData);
+
+  watch(inputData, () => {
+    data.value = inputData.filteredData;
+  });
+
+  const updateDataIndexing = (key: string, newIndexKey: string) => {
+    if (!data.value || !(key in data.value) || !(newIndexKey in data.value))
+      return;
+
+    const keys = Object.keys(data.value);
+    const index = keys.indexOf(key);
+    const newIndex = keys.indexOf(newIndexKey);
+
+    if (newIndex < 0 || newIndex >= keys.length) return;
+
+    const newKeys = [...keys];
+    newKeys.splice(index, 1);
+    newKeys.splice(newIndex, 0, key);
+
+    const reorderedData = {};
+
+    newKeys.forEach((item) => {
+      reorderedData[item] = data.value[item];
+    });
+
+    data.value = reorderedData;
+  };
+
+  return { data, updateDataIndexing };
+}
 </script>
 
 <template>
@@ -7,21 +86,21 @@ const managerData = useDataManagerStore();
     <h1 class="text-4xl">Main Content</h1>
 
     <!-- Cards -->
-    <section class="grid grid-cols-2 gap-8">
-      <div
-        v-for="(value, key) of managerData.filteredData"
-        :key="key"
-        class="bg-neutral-600 p-2 rounded-lg"
-      >
-        <h2 class="text-3xl underline capitalize">{{ key }}</h2>
-        <div v-for="(propertyValue, propertyKey) of value" :key="propertyKey">
-          <span
-            ><strong>{{ propertyKey }}:</strong> {{ propertyValue }}</span
-          >
-        </div>
-      </div>
+    <section ref="dataList" @dragover="handleDragSorting">
+      <transition-group name="card" tag="div" class="grid grid-cols-2 gap-8">
+        <BaseCard
+          v-for="(value, key) of data"
+          :key="key"
+          :data="{ value, key }"
+          :data-key="key"
+        />
+      </transition-group>
     </section>
   </main>
 </template>
 
-<style></style>
+<style>
+.card-move {
+  transition: 0.3s ease;
+}
+</style>
