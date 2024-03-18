a4b1d663c5ce1a16c46e00bde2a2ff0cf7bad34c
Tharin Wilk <Tharin.m.wilk@gmail.com>
2024-03-18 19:58:12 -0400

    Added data persistance and storage system

--- stores/managers.ts
+++ stores/managers.ts
@@ -1,27 +1,37 @@
-import testData from "~/data/test-data.json";
-import homeData from "~/data/home-data.json";
+export const useManagerStore = defineStore(
+  "Managers",
+  () => {
+    type Manager = {
+      name: string;
+      icon: string;
+      data: any;
+    };
 
-export const useManagerStore = defineStore("Managers", () => {
-  const activeManagerName = ref();
+    const managers = ref<Manager[]>([]);
 
-  const managers = ref([
-    { name: "Home", icon: "house", data: homeData },
-    { name: "Test", icon: "car", data: testData },
-  ]);
+    const activeManagerName = ref();
 
-  const addManager = (manager: any) => {
-    managers.value.push(manager);
-  };
+    const addManager = (manager: any) => {
+      managers.value.push(manager);
+    };
 
-  const setActiveManager = (managerName: string) => {
-    activeManagerName.value = managerName;
-  };
+    const setActiveManager = (managerName: string) => {
+      activeManagerName.value = managerName;
+    };
 
-  const getActiveManager = computed(() => {
-    return managers.value.find(
-      (manager) => manager.name === activeManagerName.value
-    );
-  });
+    const getActiveManager = computed(() => {
+      return managers.value.find(
+        (manager) => manager.name === activeManagerName.value
+      );
+    });
 
-  return { managers, addManager, setActiveManager, getActiveManager };
-});
+    return { managers, addManager, setActiveManager, getActiveManager };
+  },
+  {
+    persist: {
+      storage: persistedState.cookiesWithOptions({
+        maxAge: 315360000,
+      }),
+    },
+  }
+);
