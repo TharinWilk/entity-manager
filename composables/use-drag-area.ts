957babbe0578ed2d18b0378fa935264da4a974eb
Tharin Wilk <Tharin.m.wilk@gmail.com>
2025-02-05 20:42:44 -0500

    0.9.0 Updated drag area composable

--- composables/use-drag-area.ts
+++ composables/use-drag-area.ts
@@ -5,7 +5,6 @@ export const useDragArea = (element: MaybeRef) => {
   onMounted(() => {
     dropZone.value = unref(element);
     if (dropZone.value) {
-      console.log(dropZone.value);
       dropZone.value.addEventListener("dragover", handleDragOver);
       dropZone.value.addEventListener("drop", handleDrop);
     }
@@ -34,50 +33,5 @@ export const useDragArea = (element: MaybeRef) => {
     };
   };
 
-  watch(dropZone, (newValue) => {
-    console.log(newValue);
-  });
-
   return { mousePosition, handleDragOver, handleDrop };
 };
-
-// const handleDragSorting = (event: MouseEvent) => {
-//   if (!dataList.value || !managerDataStore.filter) {
-//     return;
-//   }
-
-//   const draggedElement = dataList.value.querySelector(
-//     ".card.dragging"
-//   ) as HTMLElement;
-
-//   const siblings = Array.from(
-//     dataList.value.querySelectorAll(".card:not(.dragging)")
-//   ) as HTMLElement[];
-
-//   const mouseY = event.clientY;
-//   const mouseX = event.clientX;
-
-//   for (const sibling of siblings) {
-//     const siblingRect = sibling.getBoundingClientRect();
-
-//     if (
-//       mouseY >= siblingRect.top &&
-//       mouseY <= siblingRect.bottom &&
-//       mouseX >= siblingRect.left &&
-//       mouseX <= siblingRect.right
-//     ) {
-//       if (isTransitioning.value) return;
-
-//       const elementKey = draggedElement.dataset.key;
-//       const siblingKey = sibling.dataset.key;
-//       if (!elementKey || !siblingKey) return;
-
-//       updateDataIndexing(elementKey, siblingKey);
-//       isTransitioning.value = true;
-
-//       setTimeout(() => {
-//         isTransitioning.value = false;
-//       }, 300);
-//     }
-//   }
-// };
